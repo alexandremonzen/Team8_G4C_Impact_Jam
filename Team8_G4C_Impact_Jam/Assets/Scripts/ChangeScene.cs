f3c8b308f3ddd4a61ddf91e1b2681caedce1ef1b
TERA_BITE <1.vibertron.1@gmail.com>
2022-11-10 16:05:23 -0300

    update menus

---
+++ Team8_G4C_Impact_Jam/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ChangeScene : MonoBehaviour {
+
+    public string Level;
+    public bool ChangeByCollision;
+
+    public void TrocarScene(string i)
+    {
+        Cursor.lockState = CursorLockMode.None;
+        SceneManager.LoadScene(i);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player" && ChangeByCollision == true)
+        {
+            TrocarScene(Level);
+        }
+    }
+}
